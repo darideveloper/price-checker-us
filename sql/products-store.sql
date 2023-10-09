c2656d11a49f4a3e6b2758a1ce7b70dfcce5d9fa
daridev <darideveloper@gmail.com>
2023-10-09 12:30:16 -0600

    feat (sql): add LogType and Logs tables

--- sql/products-store.sql
+++ sql/products-store.sql
@@ -1,42 +1,21 @@
-CREATE TABLE `Products` (
-  `id` bigint PRIMARY KEY AUTO_INCREMENT,
-  `image` varchar(255),
-  `title` varchar(255),
-  `rate_num` float,
-  `reviews` integer,
-  `price` float,
-  `best_seller` boolean,
-  `sales` integer,
-  `link` text,
-  `id_store` integer,
-  `id_request` integer
-);
-
-CREATE TABLE `Stores` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-);
-
-CREATE TABLE `ApiKeys` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `api_key` varchar(255),
-  `is_active` bool
-);
-
-CREATE TABLE `Requests` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `keyword` VARCHAR(200) NOT NULL,
-  `status` integer,
-  `todo_datetime` datetime,
-  `working_datetime` datetime,
-  `done_datetime` datetime,
-  `api_key` integer
-);
 
 CREATE TABLE `Status` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255)
+);
+
+CREATE TABLE `LogsType` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255)
+);
+
+CREATE TABLE `Logs` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `log_type` integer,
+  `id_store` integer,
+  `id_request` integer,
+  `api_key` integer,
+  `message` varchar(255)
 );
 
 ALTER TABLE `Products` ADD FOREIGN KEY (`id_store`) REFERENCES `Stores` (`id`);
@@ -46,3 +25,11 @@ ALTER TABLE `Products` ADD FOREIGN KEY (`id_request`) REFERENCES `Requests` (`id
 ALTER TABLE `Requests` ADD FOREIGN KEY (`status`) REFERENCES `Status` (`id`);
 
 ALTER TABLE `Requests` ADD FOREIGN KEY (`api_key`) REFERENCES `ApiKeys` (`id`);
+
+ALTER TABLE `Logs` ADD FOREIGN KEY (`log_type`) REFERENCES `LogsType` (`id`);
+
+ALTER TABLE `Logs` ADD FOREIGN KEY (`id_store`) REFERENCES `Stores` (`id`);
+
+ALTER TABLE `Logs` ADD FOREIGN KEY (`id_request`) REFERENCES `Requests` (`id`);
+
+ALTER TABLE `Logs` ADD FOREIGN KEY (`api_key`) REFERENCES `ApiKeys` (`id`);
