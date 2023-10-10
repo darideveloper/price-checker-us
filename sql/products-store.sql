b59f1d205a4cccf770949ab93a9c3a1d33c3d5f4
daridev <darideveloper@gmail.com>
2023-10-10 13:37:38 -0600

    feat (sql): update file

--- sql/products-store.sql
+++ sql/products-store.sql
@@ -1,30 +1,321 @@
-CREATE TABLE `log_types` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2023 at 09:20 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_scraping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apikeys`
+--
+
+CREATE TABLE `apikeys` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `api_key` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `apikeys`
+--
+
+INSERT INTO `apikeys` (`id`, `name`, `api_key`, `is_active`) VALUES
+(1, 'test', 'Sp6pM3VzDN5wIm6bqQJ8', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_log_type` int(11) DEFAULT NULL,
+  `id_log_origin` int(11) DEFAULT NULL,
+  `id_store` int(11) DEFAULT NULL,
+  `id_request` int(11) DEFAULT NULL,
+  `id_api_key` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_origins`
+--
 
 CREATE TABLE `log_origins` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
-);
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `Logs` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `timestamp` timestamp,
-  `id_log_type` integer,
-  `id_log_origin` integer,
-  `id_store` integer,
-  `id_request` integer,
-  `id_api_key` integer,
-  `message` varchar(255)
-);
+--
+-- Dumping data for table `log_origins`
+--
 
-ALTER TABLE `Logs` ADD FOREIGN KEY (`id_log_type`) REFERENCES `log_types` (`id`);
+INSERT INTO `log_origins` (`id`, `name`) VALUES
+(1, 'api'),
+(2, 'database'),
+(3, 'scraper'),
+(5, 'crawlbase');
 
-ALTER TABLE `Logs` ADD FOREIGN KEY (`id_log_origin`) REFERENCES `log_origins` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Logs` ADD FOREIGN KEY (`id_store`) REFERENCES `Stores` (`id`);
+--
+-- Table structure for table `log_types`
+--
 
-ALTER TABLE `Logs` ADD FOREIGN KEY (`id_request`) REFERENCES `Requests` (`id`);
+CREATE TABLE `log_types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Logs` ADD FOREIGN KEY (`id_api_key`) REFERENCES `ApiKeys` (`id`);
+--
+-- Dumping data for table `log_types`
+--
+
+INSERT INTO `log_types` (`id`, `name`) VALUES
+(1, 'info'),
+(2, 'error');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `rate_num` float DEFAULT NULL,
+  `reviews` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `best_seller` tinyint(1) DEFAULT NULL,
+  `sales` int(11) DEFAULT NULL,
+  `link` text DEFAULT NULL,
+  `id_store` int(11) DEFAULT NULL,
+  `id_request` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` int(11) NOT NULL,
+  `keyword` varchar(200) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `todo_datetime` datetime DEFAULT NULL,
+  `working_datetime` datetime DEFAULT NULL,
+  `done_datetime` datetime DEFAULT NULL,
+  `api_key` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'to do'),
+(2, 'working'),
+(3, 'done');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`) VALUES
+(1, 'amazon'),
+(3, 'ebay'),
+(5, 'walmart');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `apikeys`
+--
+ALTER TABLE `apikeys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_log_type` (`id_log_type`),
+  ADD KEY `id_log_origin` (`id_log_origin`),
+  ADD KEY `id_store` (`id_store`),
+  ADD KEY `id_request` (`id_request`),
+  ADD KEY `id_api_key` (`id_api_key`);
+
+--
+-- Indexes for table `log_origins`
+--
+ALTER TABLE `log_origins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `log_types`
+--
+ALTER TABLE `log_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_store` (`id_store`),
+  ADD KEY `id_request` (`id_request`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status` (`status`),
+  ADD KEY `api_key` (`api_key`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apikeys`
+--
+ALTER TABLE `apikeys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=398;
+
+--
+-- AUTO_INCREMENT for table `log_origins`
+--
+ALTER TABLE `log_origins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `log_types`
+--
+ALTER TABLE `log_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3126;
+
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_log_type`) REFERENCES `log_types` (`id`),
+  ADD CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`id_log_origin`) REFERENCES `log_origins` (`id`),
+  ADD CONSTRAINT `logs_ibfk_3` FOREIGN KEY (`id_store`) REFERENCES `stores` (`id`),
+  ADD CONSTRAINT `logs_ibfk_4` FOREIGN KEY (`id_request`) REFERENCES `requests` (`id`),
+  ADD CONSTRAINT `logs_ibfk_5` FOREIGN KEY (`id_api_key`) REFERENCES `apikeys` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_store`) REFERENCES `stores` (`id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`id_request`) REFERENCES `requests` (`id`);
+
+--
+-- Constraints for table `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`status`) REFERENCES `status` (`id`),
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`api_key`) REFERENCES `apikeys` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
